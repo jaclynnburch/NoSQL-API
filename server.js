2effe6ccb4f7e37210433e9b064368f65b31b789
unknown <jaclynnburch@gmail.com>
2024-02-17 08:00:05 -0800

    install mongodb

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ require('./models/user');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
-	console.log(`Server runnng on ${PORT}`);
+	console.log(`Server running on ${PORT}`);
 });
