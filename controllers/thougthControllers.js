c873b72c28109ade5830a91aeb5636635ccdffd1
unknown <jaclynnburch@gmail.com>
2024-02-16 20:04:05 -0800

    updated db

--- controllers/thougthControllers.js
+++ controllers/thougthControllers.js
@@ -1,5 +1,5 @@
-const user = require('../models/user');
-const thought = require('../models/thought');
+const user = require('./models/user');
+const thought = require('./models/thought');
 
 module.exports = {
     getAllThoughts(req, res) {
